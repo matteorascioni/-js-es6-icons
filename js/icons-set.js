73f45d2918d982633c3cfc0ffedc2cc02550cfa0
Matteo <Rascioni>
2020-11-04 17:12:11 +0100

    50%

--- js/icons-set.js
+++ js/icons-set.js
@@ -1,5 +1,7 @@
-// Icon set
-const icons = [
+// Jquery Starter 
+$(document).ready(function() {
+  // Icon set
+  const icons = [
     {
       name: 'cat',
       prefix: 'fa-',
@@ -96,4 +98,96 @@ const icons = [
       type: 'user',
       family: 'fas',
     },
-];
+  ];
+
+  const colors = [
+    'blue',
+    'orange',
+    'purple'
+  ]
+
+
+  // Icons container
+  const container = $('.icons');
+
+  // 1 Print Icon on screen
+  printIcons(icons, container);
+
+   // 2 Print Icon with color
+   const coloredIcons = colorIcons(icons, colors);
+   printIcons(coloredIcons, container);
+
+
+});  // <- End Jquery
+
+
+
+
+/***************************************************************************
+ * FUNCTIONS
+***************************************************************************/
+
+
+
+/**
+ * PRINT ICONS ON SCREEN
+ */
+function printIcons(icons, container) {
+
+  icons.forEach((icon) => {
+    const {family, prefix, name, color} = icon;
+
+    const html = 
+    `<div class="icon">
+        <i 
+          class="${family} ${prefix}${name}" 
+          style="color: ${color}">
+        </i>
+        <div class="title">${name}</div>
+    </div>`;
+
+    container.append(html);
+  });
+}
+
+
+
+/**
+ * COLORED ICONS
+ */
+function colorIcons(icons, colors) {
+  // Get types
+  const types = getTypes(icons);
+
+  // Assign color by types
+  const coloredIcons = icons.map((icon) => {
+    const indexType = types.indexOf(icon.type);
+
+    return {
+      ...icon,
+      color: colors[indexType],
+    };
+  });
+
+  return coloredIcons;
+}
+    
+
+
+/**
+ * ICON TYPE FUNCTIONS 
+ */
+function getTypes(icons) {
+  const types = [];
+
+  icons.forEach((icon) => {
+    if (! types.includes(icon.type)) {
+      types.push(icon.type);
+    }
+  });
+
+  return types;
+}
+
+
+
